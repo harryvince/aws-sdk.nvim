ab99dfbf75b619282e9f2498ffa6e2dbb4cb0f42
Harry Vince <harryavince@gmail.com>
2023-11-18 19:20:56 +0000

    fix(#1): remove newlines from preview

--- lua/aws-sdk/internal.lua
+++ lua/aws-sdk/internal.lua
@@ -1,14 +1,15 @@
 local M = {}
 
-M.remove_paragraph_tags = function(input)
+M.cleanup_summary = function(input)
     input = input:gsub('<p>', '')
     input = input:gsub('</p>', '')
+    input = input:gsub('\n', '')
     return input
 end
 
 M.preview = function(self, entry, status)
     vim.api.nvim_win_set_option(self.state.winid, "wrap", true)
-    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { M.remove_paragraph_tags(entry.summary) })
+    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { M.cleanup_summary(entry.summary) })
 end
 
 M.copy_to_clipboard = function (value)
